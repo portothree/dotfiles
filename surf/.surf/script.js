f8a43aca7f0149345e46f3ef33e6424fb15d09e3
Gustavo Porto <gustavoporto@ya.ru>
2021-11-22 10:52:15 +0000

    feat: surf script.js to allow vim key bindings

--- surf/.surf/script.js
+++ surf/.surf/script.js
@@ -1,117 +1,54 @@
-// ==UserScript==
-// @name vimkeybindings
-// @namespace renevier.fdn.fr
-// @author arno <arenevier@fdn.fr>
-// @licence GPL/LGPL/MPL
-// @description use vim keybingings (i, j, k, l, …) to navigate a web page.
-// ==/UserScript==
-
-/*
-* If you're a vim addict, and you always find yourself typing j or k in a web
-* page, then wondering why it just does not go up and down like any good
-* software, that user script is what you have been looking for.
-*/
-
-function up() {
-    if (window.scrollByLines)
-	window.scrollByLines(-4); // gecko
-    else
-	window.scrollBy(0, -48); // webkit
-}
-
-function down() {
-    if (window.scrollByLines)
-	window.scrollByLines(4); // gecko
-    else
-	window.scrollBy(0, 48); // webkit
-}
-
-function pageup() {
-    if (window.scrollByPages)
-	window.scrollByPages(-1); // gecko
-    else
-	window.scrollBy(0, 0 - _pageScroll()); // webkit
-}
-
-function pagedown() {
-    if (window.scrollByPages)
-	window.scrollByPages(1); // gecko
-    else
-	window.scrollBy(0, _pageScroll()); // webkit
-}
-
-function right() {
-    window.scrollBy(15, 0);
-}
-
-function left() {
-    window.scrollBy(-15, 0);
-}
-
-function home() {
-    window.scroll(0, 0);
-}
-
-function bottom() {
-    window.scroll(document.width, document.height);
-}
-
-// If you don't like default key bindings, customize here. 
-// if you want to use the combination 'Ctrl + b' (for example), use '^b'
-var bindings = {
-    'h' : left, 
-    'l' : right,
-    'k' : up,
-    'j' : down,
-    'g' : home,
-    'G' : bottom,
-    //'^b': pageup,
-    //'^f': pagedown,
-}
-
 function isEditable(element) {
-    
-    if (element.nodeName.toLowerCase() == "textarea")
-	return true;
+	const elNodeName = element.nodeName.toLowerCase();
+	const isTextarea = elNodeName == 'textarea';
+	const isInput = elNodeName == 'input' && element.type == 'text';
 
-    // we don't get keypress events for text input, but I don't known
-    // if it's a bug, so let's test that
-    if (element.nodeName.toLowerCase() == "input" && element.type == "text")
-	return true;
-
-    // element is editable
-    if (document.designMode == "on" || element.contentEditable == "true") {
-	return true;
-    }
-    
-    return false;
+	return (
+		document.designMode == 'on' ||
+		element.contentEditable == 'true' ||
+		isTextArea ||
+		isInput
+	);
 }
 
-function keypress(evt) {
-    var target = evt.target;
-	    
-    // if we're on a editable element, we probably don't want to catch
-    // keypress, we just want to write the typed character.
-    if (isEditable(target))
-	return;
+function vimNavigation() {
+	const directions = {
+		up: { cmd: () => window.scrollByLines(0, -4) },
+		down: { cmd: () => window.scrollByLines(0, 4) },
+		left: { cmd: () => window.scrollBy(-30, 0) },
+		right: { cmd: () => window.scrollBy(30, 0) },
+		top: { cmd: () => window.scroll(0, 0) },
+		bottom: {
+			cmd: () => window.scroll(document.width, document.height),
+		},
+	};
 
-    var key = String.fromCharCode(evt.charCode);
-    if (evt.ctrlKey) {
-	key = '^' + key;
-    }
+	const bindings = {
+		k: directions.up,
+		j: directions.down,
+		h: directions.left,
+		l: directions.right,
+		gg: directions.top,
+		G: directions.bottom,
+	};
 
-    var fun = bindings[key];
-    if (fun)
-	fun();
+	window.addEventListener(
+		'keypress',
+		function (evt) {
+			const target = evt.target;
 
+			// Ignore keypresses on a editable element
+			if (isEditable(target)) {
+				return;
+			}
+
+			const key = String.fromCharCode(evt.charCode);
+
+			const direction = bindings[key];
+			direction.cmd();
+		},
+		false
+	);
 }
 
-function _pageScroll() {
-    // Gecko algorithm
-    // ----------------
-    // The page increment is the size of the page, minus the smaller of
-    // 10% of the size or 2 lines.  
-    return window.innerHeight - Math.min(window.innerHeight / 10, 24);
-}
-
-window.addEventLister("keypress", keypress, false)
+vimNavigation();
