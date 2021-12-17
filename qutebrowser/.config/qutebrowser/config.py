3c080515b6539ec40f47f5464b99e6d656a00a0a
Gustavo Porto <gustavoporto@ya.ru>
2021-12-17 11:28:58 +0000

    feat: change qutebrowser default pages

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,6 +2,11 @@ import dracula.draw
 
 config.load_autoconfig()
 
+memex_web_path = "/home/porto/www/memex/packages/web/index.html"
+
+c.url.default_page = memex_web_path
+c.url.start_pages = ["https://calendar.google.com/u/0/r/week", "https://github.com/notifications", memex_web_path]
+
 # Colorscheme
 dracula.draw.blood(c, {
     "spacing": {
