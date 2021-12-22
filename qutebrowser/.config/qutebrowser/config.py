521ab4d4ea9320f462234df3547e9ede85b109ef
Gustavo Porto <gustavoporto@ya.ru>
2021-12-22 10:54:43 +0000

    feat: update initial pages

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -5,12 +5,13 @@ config.load_autoconfig()
 memex_web_path = "/home/porto/www/memex/packages/web/index.html"
 
 c.url.default_page = memex_web_path
-c.url.start_pages = ["https://calendar.google.com/u/0/r/week", "https://github.com/notifications", memex_web_path]
+c.url.start_pages = [
+    "https://calendar.google.com/u/0/r/week",
+    "https://github.com/notifications",
+    "https://www.indiehackers.com",
+    "https://news.ycombinator.com"
+    memex_web_path,
+]
 
 # Colorscheme
-dracula.draw.blood(c, {
-    "spacing": {
-        "vertical": 6,
-        "horizontal": 8
-    }
-})
+dracula.draw.blood(c, {"spacing": {"vertical": 6, "horizontal": 8}})
