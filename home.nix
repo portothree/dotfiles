a73b22449d9de4ee099b1c786c0f86034d66ecff
Gustavo Porto <gustavoporto@ya.ru>
2022-02-25 14:06:21 +0000

    chore: move bspwmrc to home.nix

--- home.nix
+++ home.nix
@@ -62,6 +62,30 @@
               browser: qutebrowser
             '';
           };
+          bspwm = {
+            target = ".config/bspwm/bspwmrc";
+            text = ''
+              sxhkd &
+              keynav &
+              autorandr --change
+
+              if [[ $(bspc query -M --names | head -n 1) != "eDP" ]]; then
+                bspc monitor "HDMI-0" -d I
+                bspc monitor "DP-0" -d II III IV V VI VII VIII IX X
+              fi
+
+              bspc config border_width 0.5
+              bspc config window_gap 2
+              bspc config split_ratio 0.52
+              bspc config bordeless_monocle true
+              bspc config gapless_monocle true
+
+              unclutter -idle 1 -root
+              xdotool mousemove 999999 999999
+
+              picom --config $HOME/.config/picom/picom.conf &
+            '';
+          };
         };
 	};
 	nixpkgs = {
