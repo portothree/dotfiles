9774d7d64d60eb7812e9407ce69924def6a8d13e
Gustavo Porto <gustavoporto@ya.ru>
2022-03-02 17:07:04 +0000

    chore: add base16 dark theme to alacritty

--- hosts/gesonel/home.nix
+++ hosts/gesonel/home.nix
@@ -115,6 +115,30 @@ in {
             background = "0x181818";
             foreground = "0xd8d8d8";
           };
+          cursor = {
+            text = "0xd8d8d8";
+            cursor = "0xd8d8d8";
+          };
+          normal = {
+            black = "0x181818";
+            red = "0xab4642";
+            green = "0xaa1b56c";
+            yellow = "0xf7ca88";
+            blue = "0x7cafc2";
+            magenta = "0xba8baf";
+            cyan = "0x86c1b9";
+            white = "0xd8d8d8";
+          };
+          bright = {
+            black = "0x585858";
+            red = "0xab4642";
+            green = "0xa1b56c";
+            yellow = "0xf7ca88";
+            blue = "0x7ca88";
+            magenta = "0xba8baf";
+            cyan = "0x86c1b9";
+            white = "0xf8f8f8";
+          };
         };
       };
     };
