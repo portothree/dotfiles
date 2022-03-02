a25f2e9985bb50674f8414e49bb7e6356afd4f90
Gustavo Porto <gustavoporto@ya.ru>
2022-03-02 15:12:50 +0000

    chore: add danubio host

---
+++ config/nixos/hosts/danubio/home.nix
@@ -0,0 +1,152 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ../../../home/common.nix ];
+  home = {
+    packages = with pkgs; [
+      st
+      sysz
+      ranger
+      tig
+      google-cloud-sdk
+      k9s
+      kubectl
+      ripgrep
+      xclip
+      xdotool
+      pywal
+      astyle
+      shfmt
+      nixfmt
+      glow
+      unifont
+      fira-code
+      siji
+      weechat
+      pgcli
+      mycli
+      wuzz
+      websocat
+      weechat
+    ];
+  };
+  programs = {
+    htop = { enable = true; };
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      sessionVariables = {
+        PROMPT = "%(?.%F{green}.%F{red})λ%f %B%F{cyan}%~%f%b ";
+        VISUAL = "vim";
+        EDITOR = "vim";
+        HISTTIMEFORMAT = "%F %T ";
+        PATH =
+          "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/nix/var/nix/profiles/default/bin:/home/porto/nix-profile/bin";
+        NIX_PATH =
+          "/home/porto/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels";
+        LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive";
+      };
+      localVariables = { MEMEX = "/home/porto/www/memex"; };
+      shellAliases = {
+        r = "ranger";
+        rgf = "rg --files | rg";
+        ksns =
+          "kubectl api-resources --verbs=list --namespaced -o name | xargs -n1 kubectl get '$@' --show-kind --ignore-not-found";
+        krns =
+          "kubectl api-resources --namespaced=true --verbs=delete -o name | tr '' ',' | sed -e 's/,$//''";
+        kdns = "kubectl delete '$(krns)' --all";
+      };
+      initExtraFirst = ''
+        [[ /usr/local/bin/kubectl ]] && source <(kubectl completion zsh)
+      '';
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "git-auto-fetch" ];
+        theme = "robbyrussell";
+      };
+    };
+    vim = {
+      enable = true;
+      settings = {
+        background = "dark";
+        number = true;
+        tabstop = 4;
+        shiftwidth = 4;
+      };
+      plugins = with pkgs.vimPlugins; [ vimwiki ];
+      extraConfig = ''
+        set clipboard=unnamedplus
+        set t_Co=256
+        set autoindent
+        set nocp
+        filetype plugin indent on
+        syntax on 
+      '';
+    };
+    git = {
+      enable = true;
+      userName = "Gustavo Porto";
+      userEmail = "gustavoporto@ya.ru";
+      extraConfig = {
+        core = { editor = "vim"; };
+        color = { ui = true; };
+        push = { default = "simple"; };
+        pull = { ff = "only"; };
+        init = { defaultBranch = "master"; };
+      };
+      delta = {
+        enable = true;
+        options = {
+          enable = true;
+          options = {
+            navigate = true;
+            line-numbers = true;
+            syntax-them = "Github";
+          };
+        };
+      };
+      ignores = [ "__pycache__" ];
+    };
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "https";
+        editor = "vim";
+        aliases = {
+          co = "pr checkout";
+          pv = "pr view";
+        };
+      };
+    };
+    tmux = {
+      enable = true;
+      clock24 = true;
+      keyMode = "vi";
+      plugins = with pkgs.tmuxPlugins; [ sensible yank ];
+      extraConfig = ''
+        set -g mouse off 
+        bind h select-pane -L
+        bind j select-pane -D
+        bind k select-pane -U
+        bind l select-pane -R
+      '';
+    };
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    fzf = {
+      enable = true;
+      defaultCommand = "rg --files | fzf";
+      enableZshIntegration = true;
+    };
+    bat = {
+      enable = true;
+      config = {
+        theme = "Github";
+        italic-text = "always";
+      };
+    };
+  };
+}
--- config/nixos/hosts/jorel/README.md
+++ config/nixos/hosts/jorel/README.md
@@ -2,7 +2,7 @@
 
 ## Overview
 
-Machine running as a VM inside Proxmox VE, it's used for work and have a tailoired dev environment for my client needs.
+Main workstation.
 
 ## Specs
 
