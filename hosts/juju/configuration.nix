7d6f5ac7be89cb4dd4dae0f0fe247a41aa04a042
Gustavo Porto <gustavoporto@ya.ru>
2022-03-03 23:53:20 +0000

    chore: juju configuration format

--- hosts/juju/configuration.nix
+++ hosts/juju/configuration.nix
@@ -2,41 +2,16 @@
 
 {
   imports = [ ./hardware-configuration.nix ];
-
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   time.timeZone = "Europe/Lisbon";
-
-  networking.useDHCP = false;
-  networking.interfaces.wlp1s0.useDHCP = true;
-
-  services.xserver.enable = true;
-
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
-  services.xserver.libinput.enable = true;
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  users.users.porto = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-  };
-
-  environment.systemPackages = with pkgs; [ wget ];
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  system.stateVersion = "21.11";
-
   networking = {
+    useDHCP = false;
+    interfaces = {
+      wlp1s0 = {
+        useDHCP = true;
+      };
+    };
     hostName = "juju";
     wireless = {
       enable = true;
@@ -50,4 +25,27 @@
       };
     };
   };
+  services.xserver.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      layout = "us";
+      libinput = {
+        enable = true;
+      };
+    };
+  };
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  users.users.porto = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+  environment.systemPackages = with pkgs; [ wget ];
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  system.stateVersion = "21.11";
 }
