e2c6815f77eaca01b32685426f0245c2853454a7
Gustavo Porto <gustavoporto@ya.ru>
2022-03-26 15:44:22 +0000

    chore: add hosts to common.nix

--- hosts/common.nix
+++ hosts/common.nix
@@ -4,4 +4,10 @@
   time = {
     timeZone = "Europe/Lisbon";
   };
+  networking.extraHosts = ''
+    192.168.1.100 pve.homelab
+    192.168.1.106 lara.homelab
+    192.168.1.200 pi.hole
+    192.168.1.200 uptime.kuma
+  '';
 }
