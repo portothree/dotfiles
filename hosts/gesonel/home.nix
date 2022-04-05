05e6b79aec9a027dd2d51893bb35336da13d3a1c
Gustavo Porto <gustavoporto@ya.ru>
2022-04-05 15:14:14 +0100

    chore: add tmux to gesonel host

--- hosts/gesonel/home.nix
+++ hosts/gesonel/home.nix
@@ -151,6 +151,19 @@
         init = { defaultBranch = "master"; };
       };
     };
+    tmux = {
+      enable = true;
+      clock24 = true;
+      keyMode = "vi";
+      plugins = with pkgs.tmuxPlugins; [ sensible yank ];
+      extraConfig = ''
+        set -g mouse off 
+        bind h select-pane -L
+        bind j select-pane -D
+        bind k select-pane -U
+        bind l select-pane -R
+      '';
+    };
     fzf = {
       enable = true;
       defaultCommand = "rg --files | fzf";
