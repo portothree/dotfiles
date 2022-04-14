c66d5190b959d365c0468ac011bb46ec3c25fb25
Gustavo Porto <gustavoporto@ya.ru>
2022-04-14 11:49:04 +0100

    chore: remove channel reference from danubio

--- hosts/danubio/configuration.nix
+++ hosts/danubio/configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ../common.nix ./hardware-configuration.nix <home-manager/nixos> ];
+  imports = [ ../common.nix ./hardware-configuration.nix ];
   boot = {
     loader = {
       systemd-boot = { enable = true; };
