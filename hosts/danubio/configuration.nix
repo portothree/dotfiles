05a625597964679d758b952f184a5d3bfecb3b69
Gustavo Porto <gustavoporto@ya.ru>
2022-04-14 10:17:11 +0100

    feat: enable docker service for danubio

--- hosts/danubio/configuration.nix
+++ hosts/danubio/configuration.nix
@@ -4,12 +4,8 @@
   imports = [ ../common.nix ./hardware-configuration.nix <home-manager/nixos> ];
   boot = {
     loader = {
-	systemd-boot = {
-		enable = true;
-	};
-	efi = {
-		canTouchEfiVariables = true;
-	};
+      systemd-boot = { enable = true; };
+      efi = { canTouchEfiVariables = true; };
     };
   };
   networking = {
@@ -19,24 +15,23 @@
     extraHosts = ''
       192.168.1.100 pve
     '';
-    nameservers = [ "208.67.222.222" "208.67.220.220"];
+    nameservers = [ "208.67.222.222" "208.67.220.220" ];
     firewall = {
       allowedTCPPorts = [ 22 80 3000 8081 ];
       allowedUDPPorts = [ ];
     };
   };
-  services = {
-    openssh = { enable = true; };
-  };
+  services = { openssh = { enable = true; }; };
   users = {
     users = {
       porto = {
         isNormalUser = true;
-        extraGroups = [ "wheel" ];
+        extraGroups = [ "wheel" "docker" ];
       };
     };
   };
   environment = { systemPackages = with pkgs; [ wget ]; };
+  virtualization = { docker = { enable = true; }; };
   home-manager = {
     users = { porto = import ./home.nix { inherit config pkgs; }; };
   };
