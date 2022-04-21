92a5c0d2e580a755e1c171931fadf42736602434
Gustavo Porto <gustavoporto@ya.ru>
2022-04-21 15:58:22 +0100

    feat: add promet vim colorscheme to gesonel

--- hosts/gesonel/home.nix
+++ hosts/gesonel/home.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }:
 
+let
+  vim-promet = pkgs.vimUtils.buildVimPlugin {
+    name = "promet";
+    src = pkgs.fetchFromGitHub {
+      owner = "portothree";
+      repo = "promet.vim";
+      rev = "d630dacfefe30996f54efdc33fa442163eb9fea7";
+      sha256 = "QC+7EkRU4OGjKEzlWhbbph2pW0g7zcLquq1FrUBCG40=";
+    };
+  };
+in
 {
   imports = [
     ../../config/home-manager/common.nix
@@ -145,6 +156,7 @@
     htop = { enable = true; };
     vim = {
       enable = true;
+      plugins = [ vim-promet ];
       settings = {
         background = "dark";
         number = true;
@@ -152,6 +164,7 @@
         shiftwidth = 4;
       };
       extraConfig = ''
+        colorscheme promet
         set clipboard=unnamedplus
         set t_Co=256
         set autoindent
