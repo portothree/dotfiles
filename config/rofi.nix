d2707eb457a948508302ba93a9dd7ec6df54442a
Gustavo Porto <gustavoporto@ya.ru>
2022-06-14 21:00:36 +0100

    feat(home-manager): update rofi extraConfig

--- config/rofi.nix
+++ config/rofi.nix
@@ -3,18 +3,6 @@
 {
   programs.rofi = {
     enable = true;
-    extraConfig = ''
-      {
-      	modi: "drun,window,run,ssh";*/
-        timeout {
-            action: "kb-cancel";
-            delay:  0;
-        }
-        filebrowser {
-            directories-first: true;
-            sorting-method:    "name";
-        }
-      }
-    '';
+    extraConfig = { modi = "drun,window"; };
   };
 }
