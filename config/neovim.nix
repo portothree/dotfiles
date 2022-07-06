f0ac3bd41613e01410a30dffebcb41d15bea5183
Gustavo Porto <gustavoporto@ya.ru>
2022-07-06 18:56:36 +0100

    feat: add neovim hm file

---
+++ config/neovim.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+let
+  vim-promet = pkgs.vimUtils.buildVimPlugin {
+    name = "promet";
+    src = pkgs.fetchFromGitHub {
+      owner = "portothree";
+      repo = "promet.vim";
+      rev = "d630dacfefe30996f54efdc33fa442163eb9fea7";
+      sha256 = "QC+7EkRU4OGjKEzlWhbbph2pW0g7zcLquq1FrUBCG40=";
+    };
+  };
+in {
+  programs = {
+    neovim = {
+      enable = true;
+      plugins = [ vim-promet ];
+      extraConfig = ''
+        colorscheme promet
+        set clipboard=unnamedplus
+        set t_Co=256
+        set autoindent
+        set nocp
+        filetype plugin indent on
+        syntax on 
+      '';
+    };
+  };
+}
+
