9474fc0e7051bcf30c8ea105629999f3262d0434
Gustavo Porto <gustavo@portosanti.com>
2022-07-20 13:34:33 +0100

    chore: add DP-3 config for bspwm

--- hosts/klong/home.nix
+++ hosts/klong/home.nix
@@ -102,7 +102,8 @@
       bspwm = {
         enable = true;
         extraConfig = ''
-          bspc monitor "eDP-1" -d I II III IV V VI VII VIII IX X
+          bspc monitor "eDP-1" -d I II III
+          bspc monitor "DP-3" -d IV V VI VII VIII IX X
           bspc config border_width 0.5
           bspc config window_gap 2
           bspc config split_ratio 0.52
