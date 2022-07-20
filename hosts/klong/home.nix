1819daf155cce8ef8835ad95e39465d4c97256fe
Gustavo Porto <gustavo@portosanti.com>
2022-07-20 13:45:40 +0100

    chore: enable alacritty from programs hm options

--- hosts/klong/home.nix
+++ hosts/klong/home.nix
@@ -11,7 +11,6 @@
   home = {
     stateVersion = "22.05";
     packages = with pkgs; [
-      alacritty
       sysz
       ranger
       pass
@@ -182,6 +181,41 @@
     spotifyd = { enable = true; };
   };
   programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        colors = {
+          primary = {
+            background = "0x181818";
+            foreground = "0xd8d8d8";
+          };
+          cursor = {
+            text = "0xd8d8d8";
+            cursor = "0xd8d8d8";
+          };
+          normal = {
+            black = "0x181818";
+            red = "0xab4642";
+            green = "0xaa1b56c";
+            yellow = "0xf7ca88";
+            blue = "0x7cafc2";
+            magenta = "0xba8baf";
+            cyan = "0x86c1b9";
+            white = "0xd8d8d8";
+          };
+          bright = {
+            black = "0x585858";
+            red = "0xab4642";
+            green = "0xa1b56c";
+            yellow = "0xf7ca88";
+            blue = "0x7ca88";
+            magenta = "0xba8baf";
+            cyan = "0x86c1b9";
+            white = "0xf8f8f8";
+          };
+        };
+      };
+    };
     htop = { enable = true; };
     zsh = {
       enable = true;
