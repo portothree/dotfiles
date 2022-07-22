0219ca899be7ca14f802d1acd56085d647ae85d5
Gustavo Porto <gustavoporto@ya.ru>
2022-07-22 10:12:27 +0100

    chore: tmux yank and continuum

--- config/tmux.nix
+++ config/tmux.nix
@@ -1,11 +1,20 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.tmux = {
     enable = true;
     clock24 = true;
     keyMode = "vi";
-    plugins = with pkgs.tmuxPlugins; [ sensible yank ];
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '60'
+        '';
+      }
+      tmuxPlugins.yank
+    ];
     extraConfig = ''
       set -g mouse off 
       bind h select-pane -L
