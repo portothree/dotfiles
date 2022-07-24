d68452fff575d45fe0cc79446479ca59e0bcbcce
Gustavo Porto <gustavo@portosanti.com>
2022-07-24 15:27:57 +0100

    feat: accept submodules with extraConfig

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -3,7 +3,19 @@
 with lib;
 let
   cfg = config.modules.tmux;
-
+  pluginModule = types.submodule {
+    options = {
+      plugin = mkOption {
+        type = types.package;
+        description = "Path of the configuration file to include.";
+      };
+      extraConfig = mkOption {
+        type = types.lines;
+        description = "Additional configuration for the associated plugin.";
+        default = "";
+      };
+    };
+  };
   defaultPlugins = with pkgs; [
     {
       plugin = tmuxPlugins.continuum;
@@ -14,13 +26,17 @@ let
     }
     tmuxPlugins.yank
   ];
-
-  allPlugins = defaultPlugins ++ cfg.plugins;
+  allPlugins =
+    map (p: if types.package.check p then p else p.plugin) defaultPlugins
+    ++ cfg.plugins;
 in {
   options.modules.tmux = {
     enable = mkEnableOption "tmux";
     plugins = mkOption {
-      type = with types; listOf package;
+      type = with types;
+        listOf (either package pluginModule) // {
+          description = "List of plugin packages or submodules";
+        };
       default = [ ];
       example = literalExpression ''
         with pkgs.tmuxPlugins; [ resurrect ]
