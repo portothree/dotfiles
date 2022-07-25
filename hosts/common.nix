120c25bb653fe0ffd067a2306d5027811b5e0c69
Gustavo Porto <gustavo@portosanti.com>
2022-07-25 18:30:55 +0100

    chore: add pub cachix

--- hosts/common.nix
+++ hosts/common.nix
@@ -14,9 +14,7 @@
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [ "en_US.UTF-8/UTF-8" ];
   };
-  console = {
-    keyMap = "us";
-  };
+  console = { keyMap = "us"; };
   nix = {
     gc = {
       automatic = true;
@@ -26,6 +24,14 @@
     optimise = {
       automatic = true;
       dates = [ "weekly" ];
+    };
+    settings = {
+      substituters =
+        [ "https://cache.nixos.org/" "https://portothree.cachix.org" ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "portothree.cachix.org-1:L4w3V/jrM+5cG0yEAypCPan94GLUxWYm8VFLB774J6I="
+      ];
     };
   };
 }
