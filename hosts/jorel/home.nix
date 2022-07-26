7b48cc72d4f76fa2bbc91b7726e124fccf8915a9
Gustavo Porto <gustavo@portosanti.com>
2022-07-26 14:02:14 +0100

    chore: update jorel home.nix for display settings

--- hosts/jorel/home.nix
+++ hosts/jorel/home.nix
@@ -105,18 +105,16 @@
       bspwm = {
         enable = true;
         extraConfig = ''
-          		autorandr -l dual-normal
-
-          		bspc monitor "DP-0" -d I II III
-          		bspc monitor "HDMI-0" -d IV V VI VII VIII IX X
-          		bspc config border_width 0.5
-          		bspc config window_gap 2
-          		bspc config split_ratio 0.52
-          		bspc config bordeless_monocle true
-          		bspc config gapless_monocle true
-
-          		xdotool mousemove 999999 999999
-                    			'';
+		bspc monitor "DP-1" -d I II III
+		bspc monitor "HDMI-1" -d IV V VI VII VIII IX X
+		bspc config border_width 0.5
+		bspc config window_gap 2
+		bspc config split_ratio 0.52
+		bspc config bordeless_monocle true
+		bspc config gapless_monocle true
+		
+		xdotool mousemove 999999 999999
+	'';
       };
     };
   };
@@ -137,21 +135,21 @@
       profiles = {
         "normal-dual" = {
           fingerprint = {
-            "DP-0" =
+            "DP-1" =
               "00ffffffffffff0009d1e77845540000191e0104a5351e783a0565a756529c270f5054a56b80d1c0b300a9c08180810081c001010101023a801871382d40582c45000f282100001e000000ff004b364c30313837323031510a20000000fd00324c1e5311010a202020202020000000fc0042656e51204757323438300a20016702031cf14f901f041303120211011406071516052309070783010000023a801871382d40582c45000f282100001f011d8018711c1620582c25000f282100009f011d007251d01e206e2855000f282100001e8c0ad08a20e02d10103e96000f28210000180000000000000000000000000000000000000000000000000000008d";
-            "HDMI-0" =
+            "HDMI-1" =
               "00ffffffffffff0009d1e778455400002a1f010380351e782e0565a756529c270f5054a56b80d1c0b300a9c08180810081c001010101023a801871382d40582c45000f282100001e000000ff0050414d30313138363031510a20000000fd00324c1e5311000a202020202020000000fc0042656e51204757323438300a200179020322f14f901f04130312021101140607151605230907078301000065030c001000023a801871382d40582c45000f282100001f011d8018711c1620582c25000f282100009f011d007251d01e206e2855000f282100001e8c0ad08a20e02d10103e96000f282100001800000000000000000000000000000000000000000003";
 
           };
           config = {
-            "DP-0" = {
+            "DP-1" = {
               enable = true;
               primary = true;
               position = "0x0";
               mode = "1920x1080";
               rotate = "normal";
             };
-            "HDMI-0" = {
+            "HDMI-1" = {
               enable = true;
               primary = false;
               position = "1920x0";
