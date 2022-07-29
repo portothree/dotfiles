5a1f9ce13d44a9a73773ad6bf2e74ffc354de197
Gustavo Porto <gustavo@portosanti.com>
2022-07-29 19:14:52 +0100

    chore: rename script

--- bin/lemonbar/flake.nix
+++ bin/lemonbar/flake.nix
@@ -2,12 +2,12 @@
   description = "Script to start lemonbar";
   inputs = { nixpkgs = { url = "nixpkgs/nixos-unstable"; }; };
   outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.mangobar;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.start-lemonbar;
 
-    packages.x86_64-linux.mangobar = let
+    packages.x86_64-linux.start-lemonbar = let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      name = "mangobar";
+      name = "start-lemonbar";
       src = builtins.readFile ./start-lemonbar;
       script = (pkgs.writeScriptBin name src).overrideAttrs (old: {
         buildCommand = ''
