c644b937620a1ca383c5fa741f8df6c6478c06a2
Gustavo Porto <gustavo@portosanti.com>
2022-07-30 10:55:19 +0100

    refactor: create mkShellScript function to wrap functions

--- bin/flake.nix
+++ bin/flake.nix
@@ -1,23 +1,30 @@
 {
   description = "Script to start lemonbar";
   inputs = { nixpkgs = { url = "nixpkgs/nixos-unstable"; }; };
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.start-lemonbar = let
+  outputs = { self, nixpkgs }:
+    let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      name = "start-lemonbar";
-      src = builtins.readFile ./lemonbar/start-lemonbar;
-      script = (pkgs.writeScriptBin name src).overrideAttrs (old: {
-        buildCommand = ''
-          ${old.buildCommand}
-           patchShebangs $out'';
-      });
-      buildInputs = with pkgs; [ lemonbar bspwm xdo ];
-    in pkgs.symlinkJoin {
-      inherit name;
-      paths = [ script ] ++ buildInputs;
-      buildInputs = [ pkgs.makeWrapper ];
-      postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+      mkShellScript = name: srcPath: deps:
+        let
+          src = builtins.readFile srcPath;
+          script = (pkgs.writeScriptBin name src).overrideAttrs (old: {
+            buildCommand = ''
+              ${old.buildCommand}
+               patchShebangs $out'';
+          });
+        in pkgs.symlinkJoin {
+          inherit name;
+          paths = [ script ] ++ deps;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        };
+
+    in {
+      packages.${system} = {
+        start-lemonbar =
+          mkShellScript "start-lemonbar" ./lemonbar/start-lemonbar
+          (with pkgs; [ lemonbar xdo bspwm ]);
+      };
     };
-  };
 }
