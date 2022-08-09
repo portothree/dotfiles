a24ef94c0395725149db5e8505828fa5145ea014
Gustavo Porto <gustavo@portosanti.com>
2022-08-09 15:58:23 +0100

    feat: add copilot.vim

--- config/neovim.nix
+++ config/neovim.nix
@@ -1,14 +1,15 @@
 { pkgs, lib, ... }:
 let
-  pluginGitHub = ref: repo:
+  pluginGitHub =  repo: version: rev:
     pkgs.vimUtils.buildVimPluginFrom2Nix {
       pname = "${lib.strings.sanitizeDerivationName repo}";
-      version = ref;
+      inherit version;
       src = builtins.fetchGit {
         url = "https://github.com/${repo}.git";
-        ref = ref;
+        inherit rev;
       };
     };
+  vim-copilot = pluginGitHub "github/copilot.vim" "release" "6c5abda66350773ae2f8fade2e931b3beb51843f";
 in {
   programs = {
     neovim = {
@@ -17,14 +18,15 @@ in {
       vimAlias = true;
       vimdiffAlias = true;
       withNodeJs = true;
-      plugins = with pkgs.vimPlugins; [
-        vim-polyglot
-        vim-fugitive
-        vim-prettier
-        editorconfig-vim
-        YouCompleteMe
-        ale
-      ];
+      plugins = with pkgs.vimPlugins;
+        [
+          vim-polyglot
+          vim-fugitive
+          vim-prettier
+          editorconfig-vim
+          YouCompleteMe
+          ale
+        ] ++ [ vim-copilot ];
       extraConfig = builtins.concatStringsSep "\n" [''
         lua << EOF
         ${lib.strings.fileContents ./neovim/init.lua}
