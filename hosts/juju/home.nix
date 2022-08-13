50fc14d8bede5792c3123523d5a176fb437f5c29
Gustavo Porto <gustavoporto@ya.ru>
2022-08-13 10:42:50 +0100

    chore: update home.nix

--- hosts/juju/home.nix
+++ hosts/juju/home.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home = {
+    packages = with pkgs; [ gnome.gnome-tweaks ];
+  };
   xdg = { enable = true; };
   programs = {
     home-manager = { enable = true; };
