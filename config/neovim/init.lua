9a8a0f05c4054b0f468f1d23f3793880852275d9
Gustavo Porto <gustavo@portosanti.com>
2022-09-19 20:18:22 +0100

    fix: define treesitter parser install dir

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -2,7 +2,12 @@ require("onedarkpro").setup({
 	theme = "onedark_dark",
 })
 
+-- Defines a rw for treesitter in the cache dir
+local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
+vim.fn.mkdir(parser_install_dir, "p")
+
 require("nvim-treesitter.configs").setup({
+	parser_install_dir = parser_install_dir,
 	ensure_installed = {
 		"bash",
 		"typescript",
