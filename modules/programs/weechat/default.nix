48969717c0be74bcb376da1b4c1d5b2b6dd26f2e
Gustavo Porto <gustavo@portosanti.com>
2022-11-23 19:49:57 +0000

    chore: remove relay options from weechat init script

--- modules/programs/weechat/default.nix
+++ modules/programs/weechat/default.nix
@@ -25,11 +25,6 @@ in {
             ];
             scripts = cfg.scripts;
             init = ''
-              /set relay.network.ssl on
-              /set relay.network.ssl_verify off
-              /set relay.network.ipv6 off
-              /set relay.network.bind_address
-              /set relay.network.port 9001
               /set plugins.var.python.urlserver.http_port "60211"
               /set plugins.var.python.slack.files_download_location "~/Downloads/"
               /set weechat.bar.buflist.size 30
