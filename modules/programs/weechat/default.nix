29ddb74f2b2b91a3e14f0cc1e12e99daa6276074
Gustavo Porto <gustavo@portosanti.com>
2022-11-23 19:57:14 +0000

    chore: update config

--- modules/programs/weechat/default.nix
+++ modules/programs/weechat/default.nix
@@ -28,7 +28,6 @@ in {
               /set plugins.var.python.urlserver.http_port "60211"
               /set plugins.var.python.slack.files_download_location "~/Downloads/"
               /set weechat.bar.buflist.size 30
-              /set weechat.completion.default_template "%(nicks)|%(irc_channels)|%(emoji)"
 
               /alias add open_url /url_hint_replace /exec -bg tmux new-window elinks {url$1}
               /key bind meta2-11~ /open_url 1
