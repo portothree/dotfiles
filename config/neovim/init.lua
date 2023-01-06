b75dd33a0a2687936a13c7e58ab635fc987d045f
Gustavo Porto <gustavo@portosanti.com>
2023-01-06 19:45:37 +0000

    fix: treesitter re-installing every time nvim is opened

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -5,6 +5,7 @@ require("onedarkpro").setup({
 -- Defines a rw for treesitter in the cache dir
 local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
 vim.fn.mkdir(parser_install_dir, "p")
+vim.opt.runtimepath:append(parser_install_dir)
 
 require("nvim-treesitter.configs").setup({
 	parser_install_dir = parser_install_dir,
