c292978a84c3edd7c73f9f04ff73ac3b66a7f5c6
Gustavo Porto <gus@p8s.co>
2023-06-13 17:33:37 +0100

    chore: improve Nix development environment management

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@ let
     ${nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
   '';
 in mkShell {
-  buildInputs = [ git ];
+  buildInputs = [ git gptcommit ];
   shellHook = pkgs.lib.concatStringsSep "\n" [''
     export FLAKE="$(pwd)";
     export PATH="$FLAKE/bin:/${nixBin}/bin:$PATH"
