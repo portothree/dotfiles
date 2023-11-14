6b8404178fe3d5d793d585d72773abc51b2b3022
Gustavo Porto <gus@portothr.ee>
2023-11-14 11:44:11 +0000

    feat: update copilot maps

--- config/neovim/maps.lua
+++ config/neovim/maps.lua
@@ -3,3 +3,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+let g:copilot_no_tab_map = v:true
