2adba71c011c65598896b0531f302ae6f638da08
Gustavo Porto <gus@portothr.ee>
2024-05-07 17:21:05 +0100

    revert: remove rsync activation package

--- profiles/zaza/home.nix
+++ profiles/zaza/home.nix
@@ -13,19 +13,6 @@ in {
   home = {
     inherit homeDirectory username;
     stateVersion = "22.11";
-    activation = {
-      # TODO: Move to a shared/common file
-      rsync-home-manager-applications =
-        lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
-          apps_source="$genProfilePath/home-path/Applications"
-          moniker="Home Manager Trampolines"
-          app_target_base="${homeDirectory}/Applications"
-          app_target="$app_target_base/$moniker"
-          mkdir -p "$app_target"
-          ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
-        '';
-    };
     packages = with pkgs; [
       xcbuild
       python311
