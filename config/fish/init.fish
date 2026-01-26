481276fc605f7405783bd18ff5cea784ee495dbf
Gustavo Porto <gus@p8s.co>
2026-01-26 19:54:48 -0300

    feat(fish): init rbenv in interactive shells

--- config/fish/init.fish
+++ config/fish/init.fish
@@ -2,3 +2,4 @@ source ~/.nix-profile/share/asdf-vm/asdf.fish
 
 export SSH_AUTH_SOCK=/Users/gustavoporto/.bitwarden-ssh-agent.sock
 /Users/gustavoporto/.local/bin/mise activate fish | source
+status --is-interactive; and rbenv init - fish | source
