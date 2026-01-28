5ca24207d1b84d40ca11e3883f8c1d3d76bc33b9
Gustavo Porto <gus@p8s.co>
2026-01-28 12:23:48 -0300

    ✨ (config): Add opencode memory agents config

--- config/fish/init.fish
+++ config/fish/init.fish
@@ -2,4 +2,3 @@ source ~/.nix-profile/share/asdf-vm/asdf.fish
 
 export SSH_AUTH_SOCK=/Users/gustavoporto/.bitwarden-ssh-agent.sock
 /Users/gustavoporto/.local/bin/mise activate fish | source
-status --is-interactive; and rbenv init - fish | source
